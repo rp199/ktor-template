37d1a554d8f9fcc9ac63cf8875e804ad6d17d4ec
ruipinto <rui@shapegames.com>
2024-11-01 21:58:35 +0000

    Upgrade to ktor 3.0.0

--- app/src/main/kotlin/com/rp199/template/configuration/plugin/Routing.kt
+++ app/src/main/kotlin/com/rp199/template/configuration/plugin/Routing.kt
@@ -2,13 +2,16 @@ package com.rp199.template.configuration.plugin
 
 import com.rp199.template.routes.healthCheck
 import com.rp199.template.routes.sampleRoute
+import com.rp199.template.service.SampleService
 import io.ktor.server.application.Application
 import io.ktor.server.routing.routing
-import org.koin.ktor.ext.get
+import org.koin.ktor.ext.inject
 
 fun Application.configureRoutes() {
+    // cannot inject on the routes anymore https://github.com/InsertKoinIO/koin/issues/1716
+    val sampleService by inject<SampleService>()
     routing {
         healthCheck()
-        sampleRoute(sampleService = get())
+        sampleRoute(sampleService = sampleService)
     }
 }
