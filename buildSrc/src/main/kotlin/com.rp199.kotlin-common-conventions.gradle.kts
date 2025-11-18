4fdc4e79823d334904c1ff214a8bf183eb8031f7
ruipinto <rui@shapegames.com>
2025-11-18 19:45:01 +0000

    Fix upgrade

--- buildSrc/src/main/kotlin/com.rp199.kotlin-common-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/com.rp199.kotlin-common-conventions.gradle.kts
@@ -1,3 +1,4 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 /*
@@ -27,7 +28,7 @@ tasks.named<Test>("test") {
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_21.majorVersion
+    compilerOptions {
+        jvmTarget = JvmTarget.JVM_21
     }
 }
